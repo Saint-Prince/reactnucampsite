15c30091eb51843a5397bde30dd511b54d5740eb
saint-prince <luckyericliu@gmail.com>
2021-03-06 14:31:51 -0500

    added comments

--- src/components/DirectoryComponent.js
+++ src/components/DirectoryComponent.js
@@ -1,5 +1,11 @@
 import React, { Component } from "react";
-import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
+import {
+  Card,
+  CardImg,
+  CardImgOverlay,
+  CardTitle,
+} from "reactstrap";
+import CampsiteInfo from "./CampsiteInfoComponent";
 
 class Directory extends Component {
   constructor(props) {
@@ -7,62 +13,36 @@ class Directory extends Component {
     super(props);
     //The constructor is the only place where you can assign a value to the state
     this.state = {
-        selectedCampsite: null
+      selectedCampsite: null,
     };
   }
-    //Below changes state when called
-    onCampsiteSelect(campsite) {
-        //Do not directly change states in react do it this way instead->
-        this.setState({selectedCampsite: campsite});
-    }
+  //Below changes state when called
+  onCampsiteSelect(campsite) {
+    //Do not directly change states in react do it this way instead->
+    this.setState({ selectedCampsite: campsite });
+  }
 
-    //Renders what the user sees when campsite is clicked
-    renderSelectedCampsite(campsite) {
-      if (campsite) {
-        return (
-          <Card>
-            <CardImg top src={campsite.image} alt={campsite.name} />
-            <CardBody>
+  render() {
+    const directory = this.props.campsites.map((campsite) => {
+      return (
+        <div key={campsite.id} className="col-md-5 m-1">
+          <Card onClick={() => this.onCampsiteSelect(campsite)}>
+            <CardImg width="100%" src={campsite.image} alt={campsite.name} />
+            <CardImgOverlay>
               <CardTitle>{campsite.name}</CardTitle>
-              <CardText>{campsite.description}</CardText>
-            </CardBody>
+            </CardImgOverlay>
           </Card>
-        );
-      }
-      //Returns empty if campsite is null -Makes sense since this will fire often?
-      return  <div/>;
-    }
+        </div>
+      );
+    });
 
-    render() {
-        const directory = this.props.campsites.map(campsite => {
-            return (
-              <div key={campsite.id} className="col-md-5 m-1">
-                <Card onClick={() => this.onCampsiteSelect(campsite)}>
-                  <CardImg
-                    width="100%"
-                    src={campsite.image}
-                    alt={campsite.name}
-                  />
-                  <CardImgOverlay>
-                    <CardTitle>{campsite.name}</CardTitle>
-                  </CardImgOverlay>
-                </Card>
-              </div>
-            );
-        });
-
-        return (
-            <div className="container">
-                <div className="row">
-                    {directory}
-                </div>
-                <div className="col-md-5 m-1">
-                  {this.renderSelectedCampsite(this.state.selectedCampsite)}
-                </div>
-            </div>
-        );
-    }
+    return (
+      <div className="container">
+        <div className="row">{directory}</div>
+        <CampsiteInfo campsite={this.state.selectedCampsite} />
+      </div>
+    );
+  }
 }
-
 
 export default Directory;
