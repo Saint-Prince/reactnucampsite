6f8d610badad2722e872c03346618c260ea9f291
saint-prince <luckyericliu@gmail.com>
2021-03-13 00:07:50 -0500

    Adding to the SPA

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -3,30 +3,48 @@ import Header from "./HeaderComponent";
 import Directory from "./DirectoryComponent";
 import Footer from "./FooterComponent";
 import Home from "./HomeComponent";
+import Contact from './ContactComponent';
 import { Switch, Route, Redirect } from "react-router-dom";
-import { CAMPSITES } from "../shared/campsites";
 import CampsiteInfo from "./CampsiteInfoComponent";
+import { CAMPSITES } from "../shared/campsites";
+import { COMMENTS } from "../shared/comments";
+import { PARTNERS } from "../shared/partners";
+import { PROMOTIONS } from "../shared/promotions";
+
 
 class Main extends Component {
   constructor(props) {
     super(props);
     this.state = {
       campsites: CAMPSITES,
+      comments: COMMENTS,
+      partners: PARTNERS,
+      promotions: PROMOTIONS
     };
   }
 
   render() {
     const HomePage = () => {
-      return <Home />;
+      return (<Home 
+                campsite={this.state.campsites.filter(campsite => campsite.featured)[0]}
+                comment={this.state.comments.filter(comment => comment.featured)[0]}
+                partner={this.state.partners.filter(partner => partner.featured)[0]}
+                promotion={this.state.promotions.filter(promotion => promotion.featured)[0]}
+              />);
     };
     return (
       <div>
         <Header />
         <Switch>
           <Route path="/home" component={HomePage} />
-          <Route exact path='/directory' render={() => <Directory campsites={this.state.campsites} />} />
+          <Route
+            exact
+            path="/directory"
+            render={() => <Directory campsites={this.state.campsites} />}
+          />
+          <Route exact path="/contactus" component={Contact} />
+          <Redirect to="/home" />
         </Switch>
-          <Redirect to='/home' />
         <Footer />
       </div>
     );
