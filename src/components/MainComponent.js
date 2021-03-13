00102414b4e9bb4c15b4ddf20052cad1c3441fad
saint-prince <luckyericliu@gmail.com>
2021-03-12 21:21:29 -0500

    added react router

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,7 +1,9 @@
 import React, { Component } from "react";
-import  Header from './HeaderComponent';
+import Header from "./HeaderComponent";
 import Directory from "./DirectoryComponent";
-import Footer from './FooterComponent';
+import Footer from "./FooterComponent";
+import Home from "./HomeComponent";
+import { Switch, Route, Redirect } from "react-router-dom";
 import { CAMPSITES } from "../shared/campsites";
 import CampsiteInfo from "./CampsiteInfoComponent";
 
@@ -10,23 +12,21 @@ class Main extends Component {
     super(props);
     this.state = {
       campsites: CAMPSITES,
-      selectedCampsite: null
     };
   }
 
-  onCampsiteSelect(campsite) {
-    this.setState({ selectedCampsite: campsite });
-  }
-
   render() {
+    const HomePage = () => {
+      return <Home />;
+    };
     return (
       <div>
         <Header />
-        <Directory
-          campsites={this.state.campsites}
-          onClick={campsiteId => this.onCampsiteSelect(campsiteId)}
-        />
-        <CampsiteInfo campsite={this.state.campsites.filter(campsite => campsite.id === this.state.selectedCampsite)[0]} />
+        <Switch>
+          <Route path="/home" component={HomePage} />
+          <Route exact path='/directory' render={() => <Directory campsites={this.state.campsites} />} />
+        </Switch>
+          <Redirect to='/home' />
         <Footer />
       </div>
     );
